87cb562cf02f7cf82e4c53996814260a4931a7cc
ca74ca <ca74ca@gmail.com>
2025-06-23 20:52:29 +0000

    Fix: ESLint flat config compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,18 @@
+// eslint.config.js
 import js from "@eslint/js";
-import ts from "typescript-eslint";
+import tseslint from "typescript-eslint";
 import next from "@next/eslint-plugin-next";
 
-/** @type {import("eslint").Linter.FlatConfig[]} */
+/** @type {import("eslint").FlatConfig[]} */
 export default [
   js.configs.recommended,
-  ...ts.configs.recommended,
-  ...ts.configs.recommendedTypeChecked,
-  next.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  next.configs.recommended,
   {
     rules: {
       "no-console": "off",
       "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    },
-    languageOptions: {
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
     },
   },
 ];
