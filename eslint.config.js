d90ab28d7efbb0a2edb00cdfce03950a28b45530
ca74ca <ca74ca@gmail.com>
2025-06-23 20:55:24 +0000

    🛠 Full push: updated files, ESLint config, and other fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,24 @@
 // eslint.config.js
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
+import ts from "typescript-eslint";
 import next from "@next/eslint-plugin-next";
 
-/** @type {import("eslint").FlatConfig[]} */
 export default [
   js.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  next.configs.recommended,
+  ...ts.configs.recommended,
+  ...ts.configs.recommendedTypeChecked,
   {
+    plugins: {
+      "@next/next": next,
+    },
     rules: {
       "no-console": "off",
       "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
     },
   },
 ];
