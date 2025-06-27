bce46bef67ae66ee0750deda72ef932ea22aa508
ca74ca <ca74ca@gmail.com>
2025-06-27 14:15:48 -0400

    Update getWhoopAuthUrl.ts

--- utils/getWhoopAuthUrl.ts
+++ utils/getWhoopAuthUrl.ts
@@ -3,13 +3,12 @@ export function getWhoopAuthUrl(wallet?: string) {
     client_id: process.env.NEXT_PUBLIC_WHOOP_CLIENT_ID!,
     redirect_uri: process.env.NEXT_PUBLIC_WHOOP_REDIRECT_URI!,
     response_type: "code",
-    scope: "read",
+    scope: "read:recovery read:cycles read:workout read:sleep read:profile read:body_measurement",
   });
 
   if (wallet) {
     params.append("state", wallet);
   }
 
-  // Use the correct endpoint
   return `https://api.prod.whoop.com/oauth/oauth2/auth?${params.toString()}`;
 }
